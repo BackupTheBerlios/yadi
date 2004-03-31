3dc0a22d1f3002be4e0e60254d8503478261a6b4
dietmarw <dietmarw>
2004-03-31 18:54:45 +0000

    logfile added fix

--- scripts/allin1.sh
+++ scripts/allin1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# allin1 $Id: allin1.sh,v 1.18 2004/03/31 18:50:59 dietmarw Exp $
+# allin1 $Id: allin1.sh,v 1.19 2004/03/31 18:54:45 dietmarw Exp $
 #
 # Copyright (c) 2004 essu, dmitri, Acoo Germany. All rights reserved.
 # Mail: acoo@berlios.de
@@ -36,7 +36,7 @@ RT=$HOME/yadi
 CVS=$RT/tuxbox-cvs		# Pfad zum CVS
 DBOX=$RT/dbox 			# Pfad zu dbox2
 IMAGES=$RT/images 		# Pfad wohin die fertigen Images (mit Datum) kopiert werden
-VERSION=" version$Revision: 1.18 $" 	# Zeilenlaenge: genau 15 Zeichen
+VERSION=" version$Revision: 1.19 $" 	# Zeilenlaenge: genau 15 Zeichen
 # Pfad zu den geaenderten und sonstigen Dateien
 CHANGE_DIR=$RT/patches/head_changed
 CHANGE_ARC_DIR=$RT/change_arcs
@@ -542,6 +542,11 @@ echo
 
 #MAIN
 prepare
+
+# Anfang Log
+{
+
+
 # Nur konfigurieren, falls kein Fehler aufgetreten ist
 if [ -e $RT/.mkheadall_error ];then
  echo
@@ -553,9 +558,6 @@ else
  configure
  patch_cvs
 fi
-
-# Anfang Log
-{
 
 make_it
 
